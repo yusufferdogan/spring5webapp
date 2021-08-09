ee4a9bccaa2a3317d924ca32c671473d700bb49b
yusuf erdogan <a.erdogan2018@gtu.edu.tr>
2021-08-09 12:44:37 +0300

    adding thymeleaf

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -3,21 +3,21 @@ package guru.springframework.spring5webapp.bootstrap;
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
 import guru.springframework.spring5webapp.domain.Publisher;
-import guru.springframework.spring5webapp.repositories.AuthorRepositories;
-import guru.springframework.spring5webapp.repositories.BookRepositories;
-import guru.springframework.spring5webapp.repositories.PublisherRepositories;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BootStrapData implements CommandLineRunner {
 
-    private final AuthorRepositories authorRepository;
-    private final BookRepositories bookRepository;
-    private final PublisherRepositories publisherRepository;
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
 
-    public BootStrapData(AuthorRepositories authorRepository, BookRepositories bookRepository, PublisherRepositories publisherRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
         this.publisherRepository = publisherRepository;
--- src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -3,6 +3,6 @@ package guru.springframework.spring5webapp.repositories;
 import guru.springframework.spring5webapp.domain.Author;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AuthorRepositories extends CrudRepository<Author,Long> {
+public interface AuthorRepository extends CrudRepository<Author,Long> {
 
 }
